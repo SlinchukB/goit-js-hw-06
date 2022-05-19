531a0e1be273fcaae1699e0db6f193b611e58441
Boris <slinchuk.b@gmail.com>
2022-05-19 13:17:09 +0300

    дз6 завд1

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,22 @@ console.log(`Number of categories: ${totalCategories.length}`);
 
 const categoriesArray = [...totalCategories]
   .map(
-    categories => `Category: ${categories.firstElementChild
+    categories => `Category: ${document.querySelector("h2")
       .textContent}
 Elements: ${categories.lastElementChild.children.length}`
   )
   .join("\n");
 console.log(categoriesArray);
 
+
+// const totalCategories = document.querySelectorAll("ul#categories>li.item");
+// console.log(`Number of categories: ${totalCategories.length}`);
+
+// const categoriesArray = [...totalCategories]
+//   .map(
+//     categories => `Category: ${categories.firstElementChild
+//       .textContent}
+// Elements: ${categories.lastElementChild.children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
